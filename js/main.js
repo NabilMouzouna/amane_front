317ded284db668f585966ca64a68cbe300568c51
NabilMouzouna <nabilmouzouna2001@gmail.com>
2022-09-13 09:34:07 +0200

    brahim dyal questionnaire dyal user 3adi mzl ma kmlat mhm 9ad 4ir hadil dyal psy

--- js/main.js
+++ js/main.js
@@ -8,44 +8,4 @@ menu.addEventListener('click',() => { navigation.classList.toggle('show-menu')})
  faq.forEach(
     (question) => { question.addEventListener('click',() =>{question.classList.toggle('question-clicked')}) }
  )
- /////////////////////////// preventing questionnaire default behavoir
- ///////////////////////////// one choice answer //////////////////
- const sgAnswers = document.querySelectorAll('#sg-choice')
- sgAnswers.forEach(
-   (answer) => { answer.addEventListener('click',() => { 
-      for(let ans of sgAnswers) {
-        ans.classList.remove('btn-active')
-      }
-      answer.classList.add('btn-active')
-     })
-}
-
- )
- ///////////////////////////// one choice answer //////////////////
-const mltAnswersLabel = document.querySelectorAll('#multi-choice label')
-const mltAnswers = document.querySelectorAll('#multi-choice')
-for(let i=0;i<mltAnswersLabel.length;i++){
-  mltAnswersLabel[i].addEventListener('click',() => { 
-    mltAnswers[i].classList.toggle('btn-active')
-   })
-}
-
-////////////////////// NExt and previous button ////////////////////////
-const nextBtn = document.querySelector("#next")
-const previousBtn = document.querySelector("#previous")
-const formOne = document.querySelector('#form-one')
-const formTwo = document.querySelector('#form-two')
-nextBtn.addEventListener('click',(e) => { 
-  e.preventDefault();
-  formOne.classList.add('hide')
-  formTwo.classList.remove('hide')
-  previousBtn.classList.remove('hide')
-  nextBtn.innerText = 'Commancer'
-})
-previousBtn.addEventListener('click',(e) => { 
-  e.preventDefault();
-  formOne.classList.remove('hide')
-  formTwo.classList.add('hide')
-  previousBtn.classList.add('hide')
-  nextBtn.innerText = 'suivant'
- })
+ 
